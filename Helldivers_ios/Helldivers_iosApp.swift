151b60ea9447b8b42d713b554c6b2cac3ebd8d67
elconnero <connerrobbins@gmail.com>
2025-10-15 10:54:59 -0700

    Initial Commit

---
+++ Helldivers_ios/Helldivers_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  Helldivers_iosApp.swift
+//  Helldivers_ios
+//
+//  Created by csuftitan on 10/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Helldivers_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Helldivers_iosTests/Helldivers_iosTests.swift
@@ -0,0 +1,17 @@
+//
+//  Helldivers_iosTests.swift
+//  Helldivers_iosTests
+//
+//  Created by csuftitan on 10/15/25.
+//
+
+import Testing
+@testable import Helldivers_ios
+
+struct Helldivers_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
